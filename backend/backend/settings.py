3e437223d39cc9b1826350550478e0e66aedf20f
RohitPatil0045 <rohitpatil0036@gmail.com>
2022-03-02 16:19:57 +0530

    Updating some modification

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -94,6 +96,11 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 
 # Password validation
